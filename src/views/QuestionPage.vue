74fbeac264f0f1b2ade10bdfe33cc9b452984066
Caitlyn McKenzie <cmckenzi@stu.southtexascollege.edu>
2024-10-25 22:19:07 -0500

    Difficulty added

--- src/views/QuestionPage.vue
+++ src/views/QuestionPage.vue
@@ -4,6 +4,7 @@ import { ref, onMounted } from 'vue'
 import useAPI from '@/composables/useAPI';
 import { useRoute } from 'vue-router';
 import BaseTitle from '@/components/BaseTitle.vue';
+import DifficultyChip from '@/components/DifficultyChip.vue';
 
 const api = useAPI()
 const question = ref(null)
@@ -49,7 +50,7 @@ const shuffle = (array) => {
             class="text-slate-700 text-4xl bg-purple-200 flex items-center justify-center rounded-lg py-10 px-2 hover:text-purple-900 trasition-colors duration-300">
           </div>
         </div>
-
+          <DifficultyChip :difficulty="question.difficulty"></DifficultyChip>
       </div>
   <div v-else class="text-slate-700" >
     Loading...
